9a69bb1d9a3624b52bd3015ed03c0a61e4756bfb
SofusVingaard <sovi0003@stud.kea.dk>
2025-04-01 11:24:59 +0200

    added db script

---
+++ src/main/resources/databasescript.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS onlywish;
+USE onlywish;
+
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(100)NOT NULL,
+                       age INT NOT NULL,
+                       email VARCHAR(250) NOT NULL,
+                       password VARCHAR(250) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wishlists (
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           user_id INT NOT NULL,
+                           name VARCHAR(100) NOT NULL,
+                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                           FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE wishes (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        wishlist_id INT NOT NULL,
+                        name VARCHAR(255) NOT NULL,
+                        description TEXT NULL,
+                        price DECIMAL(10,2) NULL,
+                        url VARCHAR(500) NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (wishlist_id) REFERENCES wishlists(id) ON DELETE CASCADE
+);
+
+CREATE TABLE reservations (
+                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              wish_id INT NOT NULL,
+                              reserver_user_id INT NOT NULL,
+                              reserved_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                              FOREIGN KEY (wish_id) REFERENCES wishes(id) ON DELETE CASCADE,
+                              FOREIGN KEY (reserver_user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
