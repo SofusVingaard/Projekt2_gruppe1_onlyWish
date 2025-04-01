beeba8e173224753545a9442e50dde90e2eb1e9e
SofusVingaard <sovi0003@stud.kea.dk>
2025-04-01 09:42:51 +0200

    oprettelse af github repo

---
+++ src/main/java/org/example/projekt2_gruppe1_onlywish/Projekt2Gruppe1OnlyWishApplication.java
@@ -0,0 +1,13 @@
+package org.example.projekt2_gruppe1_onlywish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe1OnlyWishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe1OnlyWishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projekt2_gruppe1_onlywish/Projekt2Gruppe1OnlyWishApplicationTests.java
@@ -0,0 +1,10 @@
+package org.example.projekt2_gruppe1_onlywish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe1OnlyWishApplicationTests {
+
+
+}
